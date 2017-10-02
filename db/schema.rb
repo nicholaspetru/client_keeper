1ec3812064ee5c3df3b2373301250ae99cf64be4
Nicholas Petru <nicholaspetru@gmail.com>
2017-10-02 14:38:17 -0700

    Remove cards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002200226) do
+ActiveRecord::Schema.define(version: 20171002213105) do
 
   create_table "card_products", force: :cascade do |t|
     t.string "name"
@@ -18,15 +18,6 @@ ActiveRecord::Schema.define(version: 20171002200226) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", force: :cascade do |t|
-    t.string "token"
-    t.string "user_token"
-    t.string "card_product_token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
   end
 
   create_table "clients", force: :cascade do |t|
